6eafcfde8c9a53e64e71d7b70e7f0e4b30668805
Ron Hass <ronhass37@gmail.com>
2023-11-04 02:04:38 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::io::{self, stdout};
-use std::process::Command;
+use ansi_to_tui::IntoText;
 use crossterm::{
     event,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
 };
 use ratatui::{prelude::*, widgets::*};
+use std::io::{self, stdout};
+use std::process::Command;
 use tui_textarea::{Input, Key, TextArea};
-use ansi_to_tui::IntoText;
 
 struct App<'a> {
     should_quit: bool,
@@ -31,21 +31,23 @@ impl<'a> App<'a> {
         textarea.set_block(Self::default_block());
         textarea.set_cursor_line_style(Style::default());
 
-        App{
+        App {
             prompt: textarea,
             should_quit: false,
             should_restart: false,
             display_hidden: false,
             raw_result: String::from(""),
-            result_lines: vec!{},
-            result_items: vec!{},
+            result_lines: vec![],
+            result_items: vec![],
             result_state: ListState::default(),
             result_index: None,
         }
     }
 
     fn default_block() -> Block<'static> {
-        Block::default().borders(Borders::ALL).border_type(BorderType::Rounded)
+        Block::default()
+            .borders(Borders::ALL)
+            .border_type(BorderType::Rounded)
     }
 
     fn startup(&mut self) -> io::Result<()> {
@@ -74,7 +76,7 @@ impl<'a> App<'a> {
 
                 if self.should_quit {
                     self.shutdown()?;
-                    return Ok(())
+                    return Ok(());
                 }
             }
 
@@ -85,14 +87,22 @@ impl<'a> App<'a> {
     fn handle_events(&mut self) -> io::Result<()> {
         if event::poll(std::time::Duration::from_millis(50))? {
             let should_recalc = match event::read()?.into() {
-                Input { key: Key::Char('c'), ctrl: true, .. } => {
+                Input {
+                    key: Key::Char('c'),
+                    ctrl: true,
+                    ..
+                } => {
                     self.should_quit = true;
                     false
-                },
-                Input { key: Key::Char('h'), ctrl: true, .. } => {
+                }
+                Input {
+                    key: Key::Char('h'),
+                    ctrl: true,
+                    ..
+                } => {
                     self.display_hidden = !self.display_hidden;
                     true
-                },
+                }
                 Input { key: Key::Down, .. } => {
                     self.result_index = match self.result_index {
                         None => None,
@@ -100,17 +110,26 @@ impl<'a> App<'a> {
                     };
                     self.result_state.select(self.result_index);
                     false
-                },
+                }
                 Input { key: Key::Up, .. } => {
                     self.result_index = match self.result_index {
                         None => None,
-                        Some(i) => Some((i + self.result_lines.len() - 1) % self.result_lines.len()),
+                        Some(i) => {
+                            Some((i + self.result_lines.len() - 1) % self.result_lines.len())
+                        }
                     };
                     self.result_state.select(self.result_index);
                     false
-                },
+                }
                 Input { key: Key::Esc, .. } => false,
-                Input { key: Key::Enter, .. } | Input { key: Key::Char('m'), ctrl: true, .. } => {
+                Input {
+                    key: Key::Enter, ..
+                }
+                | Input {
+                    key: Key::Char('m'),
+                    ctrl: true,
+                    ..
+                } => {
                     if let Some(i) = self.result_index {
                         let result_line = strip_ansi_escapes::strip_str(&self.result_lines[i]);
                         let mut splitted = result_line.split(":");
@@ -118,10 +137,13 @@ impl<'a> App<'a> {
                         let lineno = splitted.next().unwrap();
 
                         self.should_restart = true;
-                        let _ = Command::new("nvim").arg(format!("+{}", lineno)).arg(file).status(); 
+                        let _ = Command::new("nvim")
+                            .arg(format!("+{}", lineno))
+                            .arg(file)
+                            .status();
                     }
                     false
-                },
+                }
                 input => self.prompt.input(input),
             };
 
@@ -135,39 +157,33 @@ impl<'a> App<'a> {
     fn ui(&mut self, frame: &mut Frame) {
         let main_layout = Layout::default()
             .direction(Direction::Vertical)
-            .constraints([
-                         Constraint::Length(3),
-                         Constraint::Min(1),
-            ])
+            .constraints([Constraint::Length(3), Constraint::Min(1)])
             .split(frame.size());
 
         let top_line = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints([
-                         Constraint::Min(1),
-                         Constraint::Length(20),
-            ])
+            .constraints([Constraint::Min(1), Constraint::Length(20)])
             .split(main_layout[0]);
 
-        frame.render_widget(
-            self.prompt.widget(),
-            top_line[0],
-        );
-        let s = if self.display_hidden { "🗹 Show hidden" } else { "☐ Show hidden" };
-        frame.render_widget(
-            Paragraph::new(s).block(Self::default_block()),
-            top_line[1],
-        );
+        frame.render_widget(self.prompt.widget(), top_line[0]);
+        let s = if self.display_hidden {
+            "🗹 Show hidden"
+        } else {
+            "☐ Show hidden"
+        };
+        frame.render_widget(Paragraph::new(s).block(Self::default_block()), top_line[1]);
         frame.render_stateful_widget(
-            List::new(&*self.result_items).block(Self::default_block()).highlight_symbol("»"),
+            List::new(&*self.result_items)
+                .block(Self::default_block())
+                .highlight_symbol("»"),
             main_layout[1],
             &mut self.result_state,
         );
     }
 
     fn execute_rg(&mut self) {
-        self.result_lines = vec!{};
-        self.result_items = vec!{};
+        self.result_lines = vec![];
+        self.result_items = vec![];
         self.raw_result = String::from("");
         self.result_index = None;
         self.result_state.select(self.result_index);
@@ -180,8 +196,17 @@ impl<'a> App<'a> {
         // TODO: I copy everything here because I don't know how to work with lifetimes
         let output = self.build_rg_command(prompt_str).output().unwrap().stdout;
         self.raw_result = String::from_utf8(output).unwrap();
-        self.result_lines = self.raw_result.split("\n").filter(|s| {s.len() > 0}).map(|s| {s.to_string()}).collect();
-        self.result_items = self.result_lines.iter().map(|l| {ListItem::new(l.into_text().unwrap())}).collect();
+        self.result_lines = self
+            .raw_result
+            .split("\n")
+            .filter(|s| s.len() > 0)
+            .map(|s| s.to_string())
+            .collect();
+        self.result_items = self
+            .result_lines
+            .iter()
+            .map(|l| ListItem::new(l.into_text().unwrap()))
+            .collect();
         if self.result_lines.len() > 0 {
             self.result_index = Some(0);
             self.result_state.select(self.result_index);
@@ -201,7 +226,6 @@ impl<'a> App<'a> {
         command
     }
 }
-
 
 fn main() -> io::Result<()> {
     App::new().run()?;
