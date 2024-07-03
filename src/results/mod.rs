ec1cb4de57bfeaebb37f6aeaddf5f21df118d89d
Ron Hass <ronhass37@gmail.com>
2024-07-04 01:06:53 +0300

    make preview thread send lines

--- src/results/mod.rs
+++ src/results/mod.rs
@@ -113,7 +113,8 @@ impl<'a> Manager<'a> {
         };
 
         let (file_path, line_number) = job.get_result(index);
-        self.preview_job = Some(preview::PreviewJob::new(file_path, line_number));
+        // TODO: unwrap
+        self.preview_job = Some(preview::PreviewJob::new(file_path, line_number).unwrap());
     }
 
     pub fn update(&mut self) -> Result<bool> {
@@ -153,11 +154,15 @@ impl<'a> Manager<'a> {
             }
         }
 
-        if let Some(preview_job) = self.preview_job.as_ref() {
-            if let Some(preview) = preview_job.try_recv_preview() {
+        if let Some(preview_job) = self.preview_job.as_mut() {
+            for _ in 1..1000 {
+                let (preview, done) = preview_job.try_recv_preview()?;
                 self.selection_preview = Some(preview);
-                self.preview_job = None;
                 should_rerender = true;
+                if done {
+                    self.preview_job = None;
+                    break;
+                }
             }
         }
 
